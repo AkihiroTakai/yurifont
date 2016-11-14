944a0210939a98abca29d102295e2e412eb8dcbe
Akihiro Takai <at.sisy@gmail.com>
2016-11-14 15:45:45 +0900

    とりあえず16進数でフォントデータを吐けるようにした

---
+++ src/main.cc
@@ -0,0 +1,29 @@
+#include <opencv2/core.hpp>
+#include <stdio.h>
+
+bool is_black(cv::Mat *img, int x, int y);
+
+int main(int argc, char **argv){
+	
+	unsigned char result = 0, mask = 1;
+
+	cv::Mat src_img = cv::imread(argv[1]);
+	
+	for(int y = 0;y < 16;y++) {
+		for(int x = 0;x < 8;x++) {
+			if(is_black(&src_img, x, y)){
+				result |= (mask << x);
+			}
+		}
+		printf("%x ", result);
+	}
+
+	return 0;
+}
+
+bool is_black(cv::Mat *img, int x, int y) {
+	if(img->at<Vec3b>(y, x)[0])
+		return true;
+	else
+		return false;
+}
