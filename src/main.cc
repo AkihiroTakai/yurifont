900c20d445f56f0f2ae881b67acdf7ce05e9e886
Akihiro Takai <at.sisy@gmail.com>
2016-11-15 22:38:24 +0900

    ひとまず予定通りの動作をするようになった

--- src/main.cc
+++ src/main.cc
@@ -8,23 +8,29 @@ bool is_black(cv::Mat *img, int x, int y);
 int main(int argc, char **argv){
 	
 	unsigned char result = 0, mask = 1;
+	int count = 7;
 
 	cv::Mat src_img = cv::imread(argv[1]);
 	
 	for(int y = 0;y < 16;y++) {
 		for(int x = 0;x < 8;x++) {
 			if(is_black(&src_img, x, y)){
-				result |= (mask << x);
+				result |= (mask << count);
 			}
+			count--;
 		}
-		printf("%x ", result);
+		printf("0x%02x, ", result);
+		count = 7;
+		result = 0;
 	}
+
+	printf("\n");
 
 	return 0;
 }
 
 bool is_black(cv::Mat *img, int x, int y) {
-	if(img->at<cv::Vec3b>(y, x)[0])
+	if(img->at<cv::Vec3b>(y, x)[0] < 255)
 		return true;
 	else
 		return false;
