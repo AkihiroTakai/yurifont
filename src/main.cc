577017580a346c283ed932695ff678f9645fe23c
AkihiroTakai <at.sisy@gmail.com>
2016-12-23 14:18:40 +0900

    わざわざ関数にする必要もないなと

--- src/main.cc
+++ src/main.cc
@@ -1,20 +1,16 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgcodecs.hpp>
 #include <stdio.h>
-
-bool is_black(cv::Mat *img, int x, int y);
 
 int main(int argc, char **argv){
 
 	unsigned char result = 0, mask = 1;
 	int count = 7;
-
 	cv::Mat src_img = cv::imread(argv[1]);
 
 	for(int y = 0;y < 16;y++) {
 		for(int x = 0;x < 8;x++) {
-			if(is_black(&src_img, x, y))
+			if(src_img.at<cv::Vec3b>(y, x)[0] < 255)
 				result |= (mask << count);
 			count--;
 		}
@@ -25,8 +21,4 @@ int main(int argc, char **argv){
 	printf("\n");
 
 	return 0;
-}
-
-bool is_black(cv::Mat *img, int x, int y) {
-	return img->at<cv::Vec3b>(y, x)[0] < 255 ? true : false;
 }
